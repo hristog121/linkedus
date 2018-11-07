cc8a3f24285ae1efde8397f51549043f505e254c
Jiaqi Zheng <zhengjiaqi.jessie@gmail.com>
2018-11-07 23:52:25 +0100

    icon error fixed

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Button, Text, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
 import { createStackNavigator} from 'react-navigation';
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
@@ -64,9 +63,9 @@ export default createMaterialBottomTabNavigator(
         const { routeName } = navigation.state;
         let iconName;
         if (routeName === 'Home') {
-          iconName = `home${focused ? '' : '-outline'}`;
+          iconName = 'home';
         } else if (routeName === 'Settings') {
-          iconName = `setting${focused ? '' : '-outline'}`;
+          iconName = 'setting';
         }
         return <AntDesign name={iconName} size={25} color={tintColor} />;
       },
